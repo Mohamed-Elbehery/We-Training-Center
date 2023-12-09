e0fc228abce47cd658c9b3ed53635efbadfe6267
elsharqawi <dev.elbehery@gmail.com>
2023-12-09 04:26:46 +0200

    converting users route to js instead of ts

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,7 +1,7 @@
 import User from "@/models/user";
 import { NextResponse } from "next/server";
 
-export const GET = async (_: any, res: NextResponse) => {
+export const GET = async (_, res) => {
   try {
     const users = await User.find({});
     return NextResponse.json(users);
